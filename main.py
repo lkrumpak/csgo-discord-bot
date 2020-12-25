63d9cfd2552aa1af14873881dfb552128c36b938
lkrumpak <luka.krumpak@me.com>
2020-12-25 19:52:52 +0100

    removed rcon and added a2s

--- main.py
+++ main.py
@@ -1,79 +1,109 @@
+import asyncio
 import subprocess
-import rcon
+import json
 import discord
 from discord.ext import commands, tasks
+import a2s
 
-IP = 'iphere'
-PASSWORD = 'passwordhere'
-CHANNEL_ID = -1
-TOKEN = 'TOKENHERE'
-message_id = -1
+
+class server_info:
+    def __init__(self, config):
+        self.config = config
+        self.ip_address = config.get('server_ip')
+        self.server_port = config.get('server_port')
+
+    def get(self):
+        try:
+            server_info = a2s.info((self.ip_address, self.server_port))
+            self.server_name = server_info.server_name
+            self.connect_link = 'steam://connect/' + str(self.ip_address) + ':' + str(self.server_port) + '/'
+            self.curr_map = server_info.map_name.split('/')[-1]
+            self.players = str(server_info.player_count) + '/' + str(server_info.max_players)
+            self.ping = str(int((server_info.ping * 1000))) + 'ms'
+        except:
+            self.server_name = '-'
+            self.connect_link = 'Server is Offline :('
+            self.curr_map = 'Unknown'
+            self.players = '-1'
+            self.playerstats = 'Unknown'
+            self.ping = '-1ms'
+
+
+with open("config.json") as json_file:
+    config = json.load(json_file)
+
+client = commands.Bot(command_prefix=config.get('command_prefix'))
+server_info = server_info(config)
+server_info.get()
 reactions = ["▶️", "⏹️", "🔁"]
-client = commands.Bot(command_prefix='.')
-
-
-def retrieve_status():
-    try:
-        conn = rcon.RCONConnection(IP, password=PASSWORD)
-        response = conn.exec_command('status')
-        response = response.split('\n')
-
-        output = response[0] + '\n' + response[2].split('(')[1][:-1] + '\n' + response[5] + '\n' + response[6] + '\n'
-    except:
-        output = 'Offline'
-    return output
 
 
 @client.event
 async def on_ready():
-    global message_id
-    channel = client.get_channel(CHANNEL_ID)
-    embed = discord.Embed(title="Server [ Counter Strike : Global Offensive ]",
-                          description="Connecting...",
-                          color=discord.Color.orange())
+    channel = client.get_channel(config.get('channel_id'))
+
+    embed = discord.Embed(title='Counter Strike : Global Offensive',color=discord.Color.orange())
+    embed.add_field(name='\u200b', value='Connecting . . .', inline=False)
     msg = await channel.send(embed=embed)
-    message_id = msg.id
-    for name in reactions:
-        await msg.add_reaction(name)
-    change_status.start()
+    msg_id = {"message_id": msg.id}
+    config.update(msg_id)
+
+    for reaction in reactions:
+        await msg.add_reaction(reaction)
+
+    update_status.start()
+
 
 @client.event
 async def on_reaction_add(reaction, user):
     if user != client.user:
         if reaction.emoji == "▶️":
             subprocess.run(["./csgoserver", "start"])
-            description = 'Starting Server...'
+            description = 'Starting Server . . .'
         elif reaction.emoji == "⏹️":
             subprocess.run(["./csgoserver", "stop"])
-            description = 'Stopping Server...'
+            description = 'Stopping Server . . .'
         elif reaction.emoji == "🔁":
             subprocess.run(["./csgoserver", "restart"])
-            description = 'Restarting Server...'
+            description = 'Restarting Server . . .'
+        else:
+            await reaction.remove(user)
+            return
+    # todo: add a sleep to wait for server info and restart task\
 
         if reaction.emoji in reactions:
             color = discord.Color.orange()
-            embed = discord.Embed(title="Server [ Counter Strike : Global Offensive ]",
-                                  description=description,
-                                  color=color)
+            embed = discord.Embed(title='Counter Strike : Global Offensive', color=color)
+            embed.add_field(name='\u200b', value=description, inline=False)
             await reaction.message.edit(embed=embed)
-        await reaction.remove(user)
+        await asyncio.sleep(20)
+        update_status.restart()
 
 
-@tasks.loop(seconds=5)
-async def change_status():
-    global message_id
-    channel = client.get_channel(CHANNEL_ID)
-    msg = await channel.fetch_message(message_id)
-    status_response = retrieve_status()
+@tasks.loop(seconds=config.get('refresh_time'))
+async def update_status():
+    server_info.get()
 
-    if status_response == "Offline":
+    channel = client.get_channel(config.get('channel_id'))
+    msg = await channel.fetch_message(config.get('message_id'))
+
+    if server_info.players == '-1':
         color = discord.Color.red()
+        activity_status = 'Server Offline :('
     else:
         color = discord.Color.green()
 
-    embed = discord.Embed(title="Server [ Counter Strike : Global Offensive ]",
-                          description=status_response,
-                          color=color)
-    await msg.edit(embed=embed)
+        activity_status = server_info.curr_map + ' | ' + server_info.players
 
-client.run(TOKEN)
+    embed = discord.Embed(title='Counter Strike : Global Offensive', color=color)
+    embed.set_image(url=config.get('server_image'))
+    embed.add_field(name=server_info.server_name, value=server_info.connect_link, inline=False)
+    embed.add_field(name='Map', value=server_info.curr_map, inline=True)
+    embed.add_field(name='Players', value=server_info.players, inline=True)
+    embed.add_field(name='Ping', value=server_info.ping, inline=True)
+
+    await msg.edit(embed=embed)
+    update_status.change_interval(seconds=600)
+    await client.change_presence(activity=discord.Game(name=activity_status))
+
+client.run(config.get('token'))
---
+++ readme.md
@@ -0,0 +1,5 @@
+CSGO DISCORD BOT
+___
+The following bot is still work in progress, however, it should work. Edit the 'config.example.json'
+
+The bot is made to work with following project: https://github.com/lkrumpak/csgoserver
